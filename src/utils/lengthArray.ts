c549f263cf424b53b794e368e58ed552ba2be3f3
volshebnik09 <shulgin.grigoriy@yandex.ru>
2023-01-23 21:50:25 +0300

    add ts-node debug, fix lengthArray

--- src/utils/lengthArray.ts
+++ src/utils/lengthArray.ts
@@ -1,6 +1,6 @@
 export function lengthArray(item: any, n: number = 1) {
 	if (typeof item === 'function')
-		return Array(n).fill(item());
+		return Array(n).fill(null).map(()=>item())
 	return Array(n).fill(item);
 }
 
