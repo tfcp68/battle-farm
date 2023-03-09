22791875163369cb02b73c5414e8eb691416afe6
Alexey Geraim <oblivion25565@gmail.com>
2023-03-09 15:05:17 +0300

    some fixes

--- src/frontend/js/script.js
+++
@@ -1,9 +0,0 @@
-const cards_inner = document.querySelectorAll(".game-card__inner");
-
-cards_inner.forEach((card) => {
-    card.addEventListener("click", function () {
-        card.classList.toggle('is-flipped');
-    });
-
-});
-
