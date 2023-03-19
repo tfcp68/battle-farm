a50564bd72658c51071d874e491eec9f623e8561
Nekitech <nekitech-x@mail.ru>
2023-03-19 11:15:55 +0300

    fix isFertilizeAction

--- src/reducers/fertilize.ts
+++ src/reducers/fertilize.ts
@@ -4,7 +4,7 @@ import { isFertilizeAction, isFertilizeSubphase } from '~/src/types/guards/turnP
 
 export const reducer_Fertilize_IDLE: TTurnBasedReducer<TTurnPhase.FERTILIZE, TFertilizePhase.IDLE> = (params) => {
 	const { subPhase, context = CONTEXT_FERTILIZE, payload, action } = params;
-	if (!isFertilizeAction(action)) throw new Error(`Invalid action: ${action}`);
+	if (!isFertilizeAction()(action)) throw new Error(`Invalid action: ${action}`);
 	if (subPhase !== TFertilizePhase.IDLE)
 		throw new Error(`Fertilize/IDLE reducer is called in invalid state: ${subPhase}`);
 	switch (action) {
@@ -57,9 +57,11 @@ export const turnPhaseReducer_Fertilize: TTurnBasedReducer<TTurnPhase.FERTILIZE>
 	};
 };
 
-export const reducer_Fertilize_CROP_CONFIRM: TTurnBasedReducer<TTurnPhase.FERTILIZE, TFertilizePhase.CROP_CONFIRM> = (params) => {
+export const reducer_Fertilize_CROP_CONFIRM: TTurnBasedReducer<TTurnPhase.FERTILIZE, TFertilizePhase.CROP_CONFIRM> = (
+	params
+) => {
 	const { subPhase, context = CONTEXT_FERTILIZE, payload, action } = params;
-	if (!isFertilizeAction(action)) throw new Error(`Invalid action: ${action}`);
+	if (!isFertilizeAction()(action)) throw new Error(`Invalid action: ${action}`); // isFertilizationAction должен ведь возвращать коллбэк-функцию (?)
 	if (subPhase !== TFertilizePhase.CROP_CONFIRM)
 		throw new Error(`Fertilize/IDLE reducer is called in invalid state: ${subPhase}`);
 	switch (action) {
@@ -91,4 +93,4 @@ export const reducer_Fertilize_CROP_CONFIRM: TTurnBasedReducer<TTurnPhase.FERTIL
 				context,
 			};
 	}
-}
+};
