0b08c677e10cafa0813a438bbfcf2b5e01da60ae
Nekitech <nekitech-x@mail.ru>
2023-04-07 13:20:00 +0300

    create Bed component

--- src/UI/components/GameApp/GameApp.tsx
+++ src/UI/components/GameApp/GameApp.tsx
@@ -11,53 +11,55 @@ const GameApp = () => {
 	return (
 		<div className={styles.gameApp}>
 			{/*<GameCard cropImage={beans} />*/}
-			<GameBlockInfo
-				playerInfo={{
-					id: '1',
-					coins: 10,
-					hand: [
-						{
-							uuid: 1,
-							type: TCardType.CROP,
-							id: 'WHEAT',
-							value: 5,
-							fertilized: 10,
-							ripeTimer: 30,
-							group: TCropColor.GREEN,
-						},
-						{
-							uuid: 2,
-							type: TCardType.ACTION,
-							id: 'RED_HEAT',
-							value: 52,
-						},
-					],
-					class: TPlayerClass.WEATHER_WATCHER,
-					fertilizers: 4,
-					beds: [
-						{
-							type: TGardenBedType.COMMON,
-							crop: {
+			<div className={'container'}>
+				<GameBlockInfo
+					playerInfo={{
+						id: '1',
+						coins: 10,
+						class: TPlayerClass.WEATHER_WATCHER,
+						fertilizers: 4,
+						hand: [
+							{
+								uuid: 1,
+								type: TCardType.CROP,
 								id: 'WHEAT',
 								value: 5,
 								fertilized: 10,
 								ripeTimer: 30,
 								group: TCropColor.GREEN,
 							},
-						},
-						{
-							type: TGardenBedType.RAISED,
-							crop: {
-								id: 'CABBAGE',
-								value: 3,
-								fertilized: 2,
-								ripeTimer: 15,
-								group: TCropColor.RED,
+							{
+								uuid: 2,
+								type: TCardType.ACTION,
+								id: 'RED_HEAT',
+								value: 52,
 							},
-						},
-					],
-				}}
-			/>
+						],
+						beds: [
+							{
+								type: TGardenBedType.COMMON,
+								crop: {
+									id: 'WHEAT',
+									value: 5,
+									fertilized: 10,
+									ripeTimer: 30,
+									group: TCropColor.GREEN,
+								},
+							},
+							{
+								type: TGardenBedType.RAISED,
+								crop: {
+									id: 'CABBAGE',
+									value: 3,
+									fertilized: 2,
+									ripeTimer: 15,
+									group: TCropColor.RED,
+								},
+							},
+						],
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
--- src/UI/components/GameBeds/GameBeds.tsx
+++ src/UI/components/GameBeds/GameBeds.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '~/components/GameBlockInfo/GameBlockInfo.module.scss';
 import { TBed } from '~/src/types/serializables/crops';
+import Bed from '~/components/GameBeds/Bed/Bed';
 
 interface IGameHandProps {
 	listBeds: TBed[];
@@ -11,12 +12,7 @@ function GameBeds({ listBeds }: IGameHandProps) {
 		<ul className={styles.gameBlockInfo__list}>
 			{'Beds: '}
 			{listBeds.map((bed, ix) => {
-				return (
-					<li key={ix}>
-						{'Timer: ' + bed.crop?.ripeTimer + ' '}
-						{'Fertilize: ' + bed.crop?.fertilized}
-					</li>
-				);
+				return <Bed key={ix} bedInfo={bed} />;
 			})}
 		</ul>
 	);
