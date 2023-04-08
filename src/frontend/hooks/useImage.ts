624c2cbb22fa31273c3c96c684f2ed50af804608
Nekitech <nekitech-x@mail.ru>
2023-04-08 15:57:05 +0300

    fix

---
+++ src/frontend/hooks/useImage.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const useImage = (fileName: string) => {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [image, setImage] = useState('');
+
+	useEffect(() => {
+		const fetchImage = async () => {
+			try {
+				const response = await import(`~/assets/crops/${fileName.toLowerCase()}.png`);
+				setImage(response.default);
+			} catch (err) {
+				setError(err);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchImage();
+	}, [fileName]);
+
+	return {
+		loading,
+		error,
+		image,
+	};
+};
+
+export default useImage;
