196b1d2867174a54e018aec1807371282990b77d
Geraim <oblivion25565@gmail.com>
2023-05-06 21:18:52 +0300

    ts fixes

--- configs/assetsWebpack/assetBuilder/assetBuilder.ts
+++ configs/assetsWebpack/assetBuilder/assetBuilder.ts
@@ -58,147 +58,147 @@ Object.keys(CropCardId).forEach((c) => {
 
 export function importAll<K extends TAssetNamesDict>(
 	f: ReturnType<typeof require.context>,
-	dict: Record<TBaseAsset<K>, Record<TBaseAssetSize<K>, Partial<Record<ExtKeysT, string>>>>
+	dict: Partial<Record<TBaseAsset<K>, Record<TBaseAssetSize<K>, Partial<Record<ExtKeysT, string>>>>>
 ) {
 	f.keys().forEach((key) => {
 		const parsedPath = path.parse(f(key));
 		const pathArray = parsedPath.dir.split(path.posix.sep);
 		const fullPathToAsset = f(key);
 		const assetName = path.basename(key, '.png').toUpperCase() as TBaseAsset<K>;
-		dict[assetName][pathArray[2] as TBaseAssetSize<K>][pathArray[3].toUpperCase() as ExtKeysT] = fullPathToAsset;
+		dict[assetName]![pathArray[2] as TBaseAssetSize<K>][pathArray[3].toUpperCase() as ExtKeysT] = fullPathToAsset;
 	});
 }
 
 importAll<TAssetNamesDict.CLASSES>(
 	// @ts-ignore
 	require.context(`~/assets/classes?as=${webpSMALLClasses}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CLASSES]
+	(assetsDictionary[TAssetNamesDict.CLASSES] = {})
 );
 importAll<TAssetNamesDict.CLASSES>(
 	// @ts-ignore
 	require.context(`~/assets/classes?as=${webpLARGEClasses}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CLASSES]
+	(assetsDictionary[TAssetNamesDict.CLASSES] = {})
 );
 importAll<TAssetNamesDict.CLASSES>(
 	// @ts-ignore
 	require.context(`~/assets/classes?as=${webpLARGEClasses}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CLASSES]
+	(assetsDictionary[TAssetNamesDict.CLASSES] = {})
 );
 importAll<TAssetNamesDict.CLASSES>(
 	// @ts-ignore
 	require.context(`~/assets/classes?as=${avifSMALLClasses}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CLASSES]
+	(assetsDictionary[TAssetNamesDict.CLASSES] = {})
 );
 importAll<TAssetNamesDict.CLASSES>(
 	// @ts-ignore
 	require.context(`~/assets/classes?as=${avifLARGEClasses}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CLASSES]
+	(assetsDictionary[TAssetNamesDict.CLASSES] = {})
 );
 importAll<TAssetNamesDict.CLASSES>(
 	// @ts-ignore
 	require.context(`~/assets/classes?as=${jpegLARGEClasses}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CLASSES]
+	(assetsDictionary[TAssetNamesDict.CLASSES] = {})
 );
 importAll<TAssetNamesDict.CLASSES>(
 	// @ts-ignore
 	require.context(`~/assets/classes?as=${jpegSMALLClasses}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CLASSES]
+	(assetsDictionary[TAssetNamesDict.CLASSES] = {})
 );
 
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${webpSMALLCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${webpMEDIUMCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${webpLARGECards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${avifSMALLCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${avifMEDIUMCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${avifLARGECards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${jpegSMALLCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${jpegMEDIUMCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 importAll<TAssetNamesDict.ACTIONS>(
 	// @ts-ignore
 	require.context(`~/assets/actions?as=${jpegLARGECards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.ACTIONS]
+	(assetsDictionary[TAssetNamesDict.ACTIONS] = {})
 );
 
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${webpSMALLCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${webpMEDIUMCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${webpLARGECards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${avifSMALLCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${avifMEDIUMCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${avifLARGECards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${jpegSMALLCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${jpegMEDIUMCards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 importAll<TAssetNamesDict.CROPS>(
 	// @ts-ignore
 	require.context(`~/assets/crops?as=${jpegLARGECards}`, true, /\.png$/),
-	assetsDictionary[TAssetNamesDict.CROPS]
+	(assetsDictionary[TAssetNamesDict.CROPS] = {})
 );
 
 fs.writeFileSync(
